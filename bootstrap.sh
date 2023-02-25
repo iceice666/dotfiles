d73677050ced0d4c2361d21ab51bb96f963413b4
Brian Duan <56882049+iceice666@users.noreply.github.com>
2023-02-25 22:22:44 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 
-sudo pacman -S lolcat neofetch zsh exa
+sudo pacman -S lolcat neofetch zsh exa lazygit
 chsh -s $(which zsh)
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
@@ -9,3 +9,5 @@ cd ~/build
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+
+yay -S hyprland
