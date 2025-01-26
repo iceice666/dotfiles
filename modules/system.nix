c23600f49d1f73f764a9c235d773f021e8c5cdb8
iceice666 <iceice666@outlook.com>
2025-01-26 11:08:52 +0800

    init

---
+++ modules/system.nix
@@ -0,0 +1,68 @@
+{ pkgs, ... }:
+
+  ###################################################################################
+  #
+  #  macOS's System configuration
+  #
+  #  All the configuration options are documented here:
+  #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+  #
+  ###################################################################################
+{
+  system = {
+    stateVersion = 5;
+    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+    activationScripts.postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and apply them to the current session,
+      # so we do not need to logout and login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
+    defaults = {
+      menuExtraClock.Show24Hour = true;  # show 24 hour clock
+
+      # customize dock
+      dock = {
+        autohide = true;
+        show-recents = false;  # disable recent apps
+      };
+
+      # customize trackpad
+            trackpad = {
+              # tap - 轻触触摸板, click - 点击触摸板
+              Clicking = true;  # enable tap to click(轻触触摸板相当于点击)
+              TrackpadRightClick = true;  # enable two finger right click
+              TrackpadThreeFingerDrag = true;  # enable three finger drag
+            };
+
+    # Customize settings that not supported by nix-darwin directly
+          # see the source code of this project to get more undocumented options:
+          #    https://github.com/rgcr/m-cli
+          #
+          # All custom entries can be found by running `defaults read` command.
+          # or `defaults read xxx` to read a specific domain.
+          CustomUserPreferences = {
+            "com.apple.desktopservices" = {
+                      # Avoid creating .DS_Store files on network or USB volumes
+                      DSDontWriteNetworkStores = true;
+                      DSDontWriteUSBStores = true;
+                    };
+
+                    "com.apple.screensaver" = {
+                              # Require password immediately after sleep or screen saver begins
+                              askForPassword = 1;
+                              askForPasswordDelay = 0;
+                            };
+          };
+  };
+  };
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  # this is required if you want to use darwin's default shell - zsh
+  programs.zsh.enable = true;
+
+
+}
