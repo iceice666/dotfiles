a75db51a049e44e056e9f667e17da28533429ad6
iceice666 <iceice666@outlook.com>
2025-02-03 18:06:22 +0800

    use nix to manage plugins

--- conf/.zshrc
+++ conf/.zshrc
@@ -1,25 +1,3 @@
-
-## Setting up zsh plugins
-
-# Download Znap, if it's not there yet.
-[[ -r $HOME/znap/znap/znap.zsh ]] ||
-    git clone --depth 1 -- \
-        https://github.com/marlonrichert/zsh-snap.git $HOME/znap/znap
-
-source ~/znap/znap/znap.zsh  # Start Znap
-
-znap source marlonrichert/zsh-autocomplete
-znap source marlonrichert/zsh-edit
-
-znap source zsh-users/zsh-syntax-highlighting
-znap source hlissner/zsh-autopair
-znap source djui/alias-tips
-znap source ael-code/zsh-colored-man-pages
-znap source Freed-Wu/zsh-command-not-found
-
-znap source ohmyzsh/ohmyzsh plugins/{git,sudo,extract}
-
-
 printcat()
 {
   echo "Here is your cat.     "
--- home/zsh.nix
+++ home/zsh.nix
@@ -1,11 +1,27 @@
 {...}: {
-    environment.pathsToLink = [ "/share/zsh" ];
+  environment.pathsToLink = ["/share/zsh"];
 
   programs.zsh = {
     enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    zplug = {
+      enable = true;
+      plugins = [
+        {name = "marlonrichert/zsh-edit";}
+        {name = "marlonrichert/zsh-autocomplete";}
+        {name = "zsh-users/zsh-syntax-highlighting";}
+        {name = "hlissner/zsh-autopair";}
+        {name = "djui/alias-tips";}
+        {name = "ael-code/zsh-colored-man-pages";}
+        {name = "Freed-Wu/zsh-command-not-found";}
+        {
+          name = "plugins/{git,sudo,extract}";
+          tags = ["from:oh-my-zsh"];
+        }
+      ];
+    };
     initExtra = ''
       export PATH="$HOME/.cargo/bin:$HOME/bin:$HOME/.local/bin:$PATH"
       source ${../conf/.zshrc}
