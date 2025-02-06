1f7e52c8c6da9e775b9e4b6b8c693853ed8d91a3
iceice666 <iceice666@outlook.com>
2025-02-06 13:56:56 +0800

    add darwin settings

---
+++ hosts/MacBookM3Air/homebrew.nix
@@ -0,0 +1,42 @@
+{...}: {
+  # Managing package which doesn't included in home-manager on MacOS with homebrew
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      cleanup = "zap";
+    };
+
+    taps = [
+      "homebrew/services"
+    ];
+
+    # `brew install`
+    # Feel free to add your favorite apps here.
+    brews = [
+      "pipx"
+      "just"
+    ];
+
+    # `brew install --cask`
+    # Feel free to add your favorite apps here.
+    casks = [
+      # chatting
+      "slack"
+      "discord"
+
+      # small tools, utilities
+      "snipaste"
+      "jordanbaird-ice"
+      "stats"
+
+      # others
+      "firefox"
+      "orbstack"
+    ];
+  };
+}
