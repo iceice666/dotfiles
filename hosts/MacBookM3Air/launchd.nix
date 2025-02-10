921fc876011249228d36f21b88a95ef3daec3b72
iceice666 <iceice666@outlook.com>
2025-02-10 10:15:40 +0800

    force active every startup

--- hosts/MacBookM3Air/launchd.nix
+++ hosts/MacBookM3Air/launchd.nix
@@ -1,12 +1,24 @@
 {...}: {
-  launchd.agents = {
-    karabiner-driver = {
+  launchd.daemons = {
+    karabiner-driverkit = {
       serviceConfig = {
         Label = "org.pqrs.Karabiner-DriverKit-VirtualHIDDevice";
         RunAtLoad = true;
         KeepAlive = true;
         UserName = "root";
         Program = "/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon";
+      };
+    };
+
+    karabiner-device-manager = {
+      serviceConfig = {
+        Label = "org.pqrs.Karabiner-VirtualHIDDevice-Manager";
+        RunAtLoad = true;
+        KeepAlive = true;
+        UserName = "root";
+        ProgramArgments = [
+          "/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager" "activate"
+        ];
       };
     };
   };
