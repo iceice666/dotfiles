76d762aab497ce695eb504f4df222a923fa7d29d
Brian Duan <iceice666@outlook.com>
2025-09-11 10:48:44 +0800

    Fix andorid sdk env var

--- mba/.config/fish/conf.d/mba.fish
+++ mba/.config/fish/conf.d/mba.fish
@@ -5,11 +5,12 @@ set -gx DOTNET_ROOT /usr/local/share/dotnet/
 set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home/
 set -gx ANDROID_NDK_HOME /opt/homebrew/share/android-ndk
 set -gx ANDROID_HOME /Users/iceice666/Library/Android/sdk
+set -gx ANDROID_SDK_ROOT $ANDROID_HOME
 
 # Paths
-fish_add_path -p /opt/homebrew/share/android-commandlinetools/cmdline-tools/latest/bin/
-fish_add_path -p /opt/X11/bin
+fish_add_path -p $ANDROID_HOME/cmdline-tools/latest/bin
 fish_add_path -p $ANDROID_HOME/platform-tools
+fish_add_path -p /opt/X11/bin
 fish_add_path -p ~/.orbstack/bin
 fish_add_path -p /opt/homebrew/sbin
 fish_add_path -p /opt/homebrew/bin
