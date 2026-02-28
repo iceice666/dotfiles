dae8effbe996a8f7812e9870c601f5c7823007fc
Brian Duan <iceice666@outlook.com>
2026-02-28 10:11:42 +0800

    fix: HM require backup or force overide file

--- hosts/m3air/home/karabiner.nix
+++ hosts/m3air/home/karabiner.nix
@@ -1,31 +1,34 @@
 { ... }:
 
 {
-  home.file.".config/karabiner/karabiner.json".text = builtins.toJSON {
-    global = { };
-    profiles = [
-      {
-        name = "Default profile";
-        selected = true;
-        complex_modifications = {
-          rules = [
-            {
-              description = "CapsLock -> tap: Escape, hold: Left Control";
-              manipulators = [
-                {
-                  type = "basic";
-                  from = {
-                    key_code = "caps_lock";
-                    modifiers.optional = [ "any" ];
-                  };
-                  to = [ { key_code = "left_control"; } ];
-                  to_if_alone = [ { key_code = "escape"; } ];
-                }
-              ];
-            }
-          ];
-        };
-      }
-    ];
+  home.file.".config/karabiner/karabiner.json" = {
+    force = true;
+    text = builtins.toJSON {
+      global = { };
+      profiles = [
+        {
+          name = "Default profile";
+          selected = true;
+          complex_modifications = {
+            rules = [
+              {
+                description = "CapsLock -> tap: Escape, hold: Left Control";
+                manipulators = [
+                  {
+                    type = "basic";
+                    from = {
+                      key_code = "caps_lock";
+                      modifiers.optional = [ "any" ];
+                    };
+                    to = [ { key_code = "left_control"; } ];
+                    to_if_alone = [ { key_code = "escape"; } ];
+                  }
+                ];
+              }
+            ];
+          };
+        }
+      ];
+    };
   };
 }
