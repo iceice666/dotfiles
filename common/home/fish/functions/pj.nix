c96fd4bf3e0c85d4c3126b18dd985be095d915fa
Brian Duan <iceice666@outlook.com>
2026-04-13 00:47:28 +0800

    feat: use fzf picker for pj

--- common/home/fish/functions/pj.nix
+++ common/home/fish/functions/pj.nix
@@ -4,24 +4,30 @@
   programs.fish.functions.pj = {
     description = "Jump to project directory";
     body = ''
-      if test (count $argv) -ne 1
-          echo "Usage: pj <project-name>"
-          return 1
-      end
-
       if not test -d $ProjectDir
           echo "Project directory not found: $ProjectDir"
           return 1
       end
 
-      set target $ProjectDir/$argv[1]
-      if not test -d $target
-          echo "Project not found: $target"
-          echo "Available projects:"
-          ls $ProjectDir 2>/dev/null | head -10
+      if not type -q fzf
+          echo "fzf is required for pj"
           return 1
       end
 
+      set query (string join ' ' -- $argv)
+      set target (
+          for d in $ProjectDir/*
+              if test -d $d
+                  basename $d
+              end
+          end | fzf --query "$query"
+      )
+
+      if test -z "$target"
+          return 1
+      end
+
+      set target $ProjectDir/$target
       cd $target
     '';
   };
