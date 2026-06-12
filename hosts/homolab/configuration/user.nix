0b7986c8c49caf583e5ed68786ce36f08540737d
Brian Duan <iceice666@outlook.com>
2026-06-13 06:18:53 +0800

    fix(homolab/user): add hashed password

--- hosts/homolab/configuration/user.nix
+++ hosts/homolab/configuration/user.nix
@@ -15,6 +15,7 @@
     extraGroups = [
       "wheel"
     ];
+    hashedPassword = "$6$ZuRS1jtUzGWpge9G$2LVYNIwjEBqBwu13qWdVZpf0cVTyuyBmbop7D63nWLzEtua9Wt3377uCQMjkTqPHhfWLtRF.0dcVr/8W7F3Wr1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBv4wUd0xFUa2wRlXbB2f2IntFxTGsxwNAhGQIyxfso8 iceice666@M3Air"
     ];
